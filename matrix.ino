7e7be22e34791ee7fbb55b14c1c1291f793e455b
Илья Дубков <dubkov@skbrii.ru>
2015-03-07 19:03:02 +0600

    бегущий цветочек

--- matrix.ino
+++ matrix.ino
@@ -7,7 +7,10 @@ void setup() {
   pinMode(SS_PIN, OUTPUT);
 }
 
-byte mosinloh[8]{
+int shift=0, timer=0, timerPrev=0, len=19;
+
+byte flower[]{
+  0b00000000,
   0b00011000,
   0b00100100,
   0b01100110,
@@ -16,14 +19,33 @@ byte mosinloh[8]{
   0b01100110,
   0b00100100,
   0b00011000,
+  0b00000000,
+  0b00011000,
+  0b00100100,
+  0b01100110,
+  0b10011001,
+  0b10011001,
+  0b01100110,
+  0b00100100,
+  0b00011000,
+  0b00000000,
 };
 
 void loop() {
-  for(int row = 0; row < 8; row++) {
+
+  timer = millis();                 
+  if (timer-timerPrev>80){
+    shift++;                        
+    if (shift==len-10)shift=0;
+    timerPrev=timer;
+  }
+
+  for(int col = 0; col < 8; col++) {
     digitalWrite(SS_PIN,LOW);
-    SPI.transfer(0xFF - (1<<row));
-    SPI.transfer(mosinloh[row]);
+    SPI.transfer(0xFF-flower[col+shift]);
+    SPI.transfer(1<<(col));
     digitalWrite(SS_PIN,HIGH);
     delay(1);
   }
+
 }
